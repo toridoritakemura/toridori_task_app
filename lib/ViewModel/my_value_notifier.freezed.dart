d4748de3677c41a0be41739fdb352ab1d7d728cc
[Token発行したユーザ名] <[Token]>
2021-10-20 11:35:57 +0900

    コード修正

---
+++ lib/ViewModel/my_value_notifier.freezed.dart
@@ -0,0 +1,321 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'my_value_notifier.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$MyValueStateTearOff {
+  const _$MyValueStateTearOff();
+
+  _MyValueState call(
+      {int count = 0,
+      int tabIndex = 0,
+      bool isLoading = false,
+      bool isFilter = false,
+      bool isState = false,
+      bool isSince = false,
+      int isSort = 1,
+      List<Issue> isIssues = const []}) {
+    return _MyValueState(
+      count: count,
+      tabIndex: tabIndex,
+      isLoading: isLoading,
+      isFilter: isFilter,
+      isState: isState,
+      isSince: isSince,
+      isSort: isSort,
+      isIssues: isIssues,
+    );
+  }
+}
+
+/// @nodoc
+const $MyValueState = _$MyValueStateTearOff();
+
+/// @nodoc
+mixin _$MyValueState {
+  int get count => throw _privateConstructorUsedError; //状態管理されている実際の値
+  int get tabIndex => throw _privateConstructorUsedError;
+  bool get isLoading => throw _privateConstructorUsedError; //処理中のUI
+  bool get isFilter => throw _privateConstructorUsedError; //絞り込みボタンON/OFF
+  bool get isState =>
+      throw _privateConstructorUsedError; //Close状態のIssueを除外するチェックボックス
+  bool get isSince =>
+      throw _privateConstructorUsedError; //一年以上の更新しないIssueを除外するチェックボックス
+  int get isSort => throw _privateConstructorUsedError; //3つのRadio
+  List<Issue> get isIssues => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $MyValueStateCopyWith<MyValueState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $MyValueStateCopyWith<$Res> {
+  factory $MyValueStateCopyWith(
+          MyValueState value, $Res Function(MyValueState) then) =
+      _$MyValueStateCopyWithImpl<$Res>;
+  $Res call(
+      {int count,
+      int tabIndex,
+      bool isLoading,
+      bool isFilter,
+      bool isState,
+      bool isSince,
+      int isSort,
+      List<Issue> isIssues});
+}
+
+/// @nodoc
+class _$MyValueStateCopyWithImpl<$Res> implements $MyValueStateCopyWith<$Res> {
+  _$MyValueStateCopyWithImpl(this._value, this._then);
+
+  final MyValueState _value;
+  // ignore: unused_field
+  final $Res Function(MyValueState) _then;
+
+  @override
+  $Res call({
+    Object? count = freezed,
+    Object? tabIndex = freezed,
+    Object? isLoading = freezed,
+    Object? isFilter = freezed,
+    Object? isState = freezed,
+    Object? isSince = freezed,
+    Object? isSort = freezed,
+    Object? isIssues = freezed,
+  }) {
+    return _then(_value.copyWith(
+      count: count == freezed
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+      tabIndex: tabIndex == freezed
+          ? _value.tabIndex
+          : tabIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isFilter: isFilter == freezed
+          ? _value.isFilter
+          : isFilter // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isState: isState == freezed
+          ? _value.isState
+          : isState // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSince: isSince == freezed
+          ? _value.isSince
+          : isSince // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSort: isSort == freezed
+          ? _value.isSort
+          : isSort // ignore: cast_nullable_to_non_nullable
+              as int,
+      isIssues: isIssues == freezed
+          ? _value.isIssues
+          : isIssues // ignore: cast_nullable_to_non_nullable
+              as List<Issue>,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$MyValueStateCopyWith<$Res>
+    implements $MyValueStateCopyWith<$Res> {
+  factory _$MyValueStateCopyWith(
+          _MyValueState value, $Res Function(_MyValueState) then) =
+      __$MyValueStateCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {int count,
+      int tabIndex,
+      bool isLoading,
+      bool isFilter,
+      bool isState,
+      bool isSince,
+      int isSort,
+      List<Issue> isIssues});
+}
+
+/// @nodoc
+class __$MyValueStateCopyWithImpl<$Res> extends _$MyValueStateCopyWithImpl<$Res>
+    implements _$MyValueStateCopyWith<$Res> {
+  __$MyValueStateCopyWithImpl(
+      _MyValueState _value, $Res Function(_MyValueState) _then)
+      : super(_value, (v) => _then(v as _MyValueState));
+
+  @override
+  _MyValueState get _value => super._value as _MyValueState;
+
+  @override
+  $Res call({
+    Object? count = freezed,
+    Object? tabIndex = freezed,
+    Object? isLoading = freezed,
+    Object? isFilter = freezed,
+    Object? isState = freezed,
+    Object? isSince = freezed,
+    Object? isSort = freezed,
+    Object? isIssues = freezed,
+  }) {
+    return _then(_MyValueState(
+      count: count == freezed
+          ? _value.count
+          : count // ignore: cast_nullable_to_non_nullable
+              as int,
+      tabIndex: tabIndex == freezed
+          ? _value.tabIndex
+          : tabIndex // ignore: cast_nullable_to_non_nullable
+              as int,
+      isLoading: isLoading == freezed
+          ? _value.isLoading
+          : isLoading // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isFilter: isFilter == freezed
+          ? _value.isFilter
+          : isFilter // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isState: isState == freezed
+          ? _value.isState
+          : isState // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSince: isSince == freezed
+          ? _value.isSince
+          : isSince // ignore: cast_nullable_to_non_nullable
+              as bool,
+      isSort: isSort == freezed
+          ? _value.isSort
+          : isSort // ignore: cast_nullable_to_non_nullable
+              as int,
+      isIssues: isIssues == freezed
+          ? _value.isIssues
+          : isIssues // ignore: cast_nullable_to_non_nullable
+              as List<Issue>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_MyValueState implements _MyValueState {
+  const _$_MyValueState(
+      {this.count = 0,
+      this.tabIndex = 0,
+      this.isLoading = false,
+      this.isFilter = false,
+      this.isState = false,
+      this.isSince = false,
+      this.isSort = 1,
+      this.isIssues = const []});
+
+  @JsonKey(defaultValue: 0)
+  @override
+  final int count;
+  @JsonKey(defaultValue: 0)
+  @override //状態管理されている実際の値
+  final int tabIndex;
+  @JsonKey(defaultValue: false)
+  @override
+  final bool isLoading;
+  @JsonKey(defaultValue: false)
+  @override //処理中のUI
+  final bool isFilter;
+  @JsonKey(defaultValue: false)
+  @override //絞り込みボタンON/OFF
+  final bool isState;
+  @JsonKey(defaultValue: false)
+  @override //Close状態のIssueを除外するチェックボックス
+  final bool isSince;
+  @JsonKey(defaultValue: 1)
+  @override //一年以上の更新しないIssueを除外するチェックボックス
+  final int isSort;
+  @JsonKey(defaultValue: const [])
+  @override //3つのRadio
+  final List<Issue> isIssues;
+
+  @override
+  String toString() {
+    return 'MyValueState(count: $count, tabIndex: $tabIndex, isLoading: $isLoading, isFilter: $isFilter, isState: $isState, isSince: $isSince, isSort: $isSort, isIssues: $isIssues)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _MyValueState &&
+            (identical(other.count, count) || other.count == count) &&
+            (identical(other.tabIndex, tabIndex) ||
+                other.tabIndex == tabIndex) &&
+            (identical(other.isLoading, isLoading) ||
+                other.isLoading == isLoading) &&
+            (identical(other.isFilter, isFilter) ||
+                other.isFilter == isFilter) &&
+            (identical(other.isState, isState) || other.isState == isState) &&
+            (identical(other.isSince, isSince) || other.isSince == isSince) &&
+            (identical(other.isSort, isSort) || other.isSort == isSort) &&
+            const DeepCollectionEquality().equals(other.isIssues, isIssues));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      count,
+      tabIndex,
+      isLoading,
+      isFilter,
+      isState,
+      isSince,
+      isSort,
+      const DeepCollectionEquality().hash(isIssues));
+
+  @JsonKey(ignore: true)
+  @override
+  _$MyValueStateCopyWith<_MyValueState> get copyWith =>
+      __$MyValueStateCopyWithImpl<_MyValueState>(this, _$identity);
+}
+
+abstract class _MyValueState implements MyValueState {
+  const factory _MyValueState(
+      {int count,
+      int tabIndex,
+      bool isLoading,
+      bool isFilter,
+      bool isState,
+      bool isSince,
+      int isSort,
+      List<Issue> isIssues}) = _$_MyValueState;
+
+  @override
+  int get count;
+  @override //状態管理されている実際の値
+  int get tabIndex;
+  @override
+  bool get isLoading;
+  @override //処理中のUI
+  bool get isFilter;
+  @override //絞り込みボタンON/OFF
+  bool get isState;
+  @override //Close状態のIssueを除外するチェックボックス
+  bool get isSince;
+  @override //一年以上の更新しないIssueを除外するチェックボックス
+  int get isSort;
+  @override //3つのRadio
+  List<Issue> get isIssues;
+  @override
+  @JsonKey(ignore: true)
+  _$MyValueStateCopyWith<_MyValueState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
